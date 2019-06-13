a368b2cffea56aa210905e0b77d13644494dd1b0
Ilyas Deckers <deckersilyas@gmail.com>
2019-06-13 15:14:30 +0200

    new scripts

---
+++ checks/check_debian.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+os_type="$(lsb_release -si 2>/dev/null)"
+if [ "$os_type" = "Debian" ]; then
+  os_ver="$(sed 's/\..*//' /etc/debian_version 2>/dev/null)"
+  if [ "$os_ver" != "8" ]; then
+    echoerr "Only supports Debian 8 (Jessie)"
+    exit 1
+  fi
+fi
---
+++ checks/check_root.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+if [ "$(id -u)" != 0 ]; then
+  echo "This script must be run as root. 'sudo bash $0'"
+  exit 1
+fi
